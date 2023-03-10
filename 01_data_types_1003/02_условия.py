1add360b6c23792d7425b8d5d53775e8d82461f1
greatraksin <greatraksin@icloud.com>
2023-03-10 16:55:30 +0300

    Initial commit

---
+++ 01_data_types_1003/02_условия.py
@@ -0,0 +1,9 @@
+import random
+
+a = random.randint(1, 350)
+print(a)
+if ((a / 5) >= 20):  # если результат деления а на 5 будет больше 20
+    print('OK')
+else:  # иначе (если результат будет меньше 20)
+    print('NE OK')
+
